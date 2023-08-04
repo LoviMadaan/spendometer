9071669ca87e976982fdf5ff2dd5ed9580223c1b
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-08-04 20:39:07 +0530

    Views and devise updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'groups#index'
+  resources :groups, only: [:index, :show, :new, :create, :destroy] do
+    resources :entities, only: [:index]
+  end
+
+  resources :entities, only: [:new, :create, :destroy]
 end
