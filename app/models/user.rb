e399dd2d157943939a67a5eba3d5763d5ea0b910
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-08-05 15:52:18 +0530

    Style updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
     validates :name, presence: true, length: { maximum: 100 }
-
+    validates :email, presence: true
+    validates :password, presence: true
+    
     has_many :groups, dependent: :delete_all
-    has_many :entities, dependent: :delete_all
+    has_many :spendings, dependent: :delete_all
 end
